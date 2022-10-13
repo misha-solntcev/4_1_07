6afd41e2a1f42d5ae86c9400d08e42af34d61523
misha-solntcev <misha.solntcev@gmail.com>
2022-10-13 10:04:25 +0800

    Окончательный вариант.

--- Program.cs
+++ Program.cs
@@ -19,11 +19,12 @@ namespace _4_1_07
         {
             int[,] array = new int[,]
             {
-                { 8, 0, 6, -4, 8 },
-                { 4, 1, 6, 7, 8 },
-                { 7, 8, 9, 0, 2 },
-                { 12, 0, 3, -4, 5 },
-                { 13, 0, 6, 7, 8 }                
+                { 8, 0, 6, -4, 8, 1 },
+                { 4, 1, 36, 7, 8, 2 },
+                { 7, 8, 9, 0, 2, 3 },
+                { 12, 0, 3, -4, 5, 4 },
+                { 13, 0, 6, 7, 8, 5 },
+                { 14, 8, 9, 0, 2, 3 },
             };
 
             // Вывод в консоль исходного массива.
@@ -58,24 +59,19 @@ namespace _4_1_07
             Console.WriteLine();
 
             // Определяем и активируем массив списков для записи элементов диагоналей.
-            List<int>[] diagonals = new List<int>[(array.GetLength(0) - 2)*2];
-            diagonals[0] = new List<int>();
-            diagonals[1] = new List<int>();
-            diagonals[2] = new List<int>();
-            diagonals[3] = new List<int>();
-            diagonals[4] = new List<int>();
-            diagonals[5] = new List<int>();
-            
-            for (int k = 1; k < array.GetLength(0) - 1; k++)                            
+            List<int>[] diagonals = new List<int>[array.GetLength(0) + 4];
+            for (int i = 0; i < diagonals.Length; i++)
+                diagonals[i] = new List<int>();            
+            // Записываем элементы диагоналей в массив списков.
+            for (int k = 1; k < array.GetLength(0) ; k++)                            
                 for (int i = 0; i < array.GetLength(0); i++)                                    
                     for (int j = 0; j < array.GetLength(1); j++)
                     {
                         if (i == j + k)
                             diagonals[k - 1].Add(array[i, j]);
                         if (j == i + k)
-                            diagonals[k + 2].Add(array[i, j]);
+                            diagonals[k + 4].Add(array[i, j]);
                     }
-                        
             // Вывод в консоль элементов диагоналей.
             Console.WriteLine("Элементы диагоналей:");
             for (int i = 0; i < diagonals.Length; i++)
